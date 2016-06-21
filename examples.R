5afb0994a55f029812324a5db559d5fcb8022bcd
Jacob Carey <JCarey@telligen.com>
2016-06-21 17:25:19 -0400

    Work on dynamic model more

--- examples.R
+++ examples.R
@@ -24,7 +24,7 @@ for (i in seq(2, length(Y))) {
 }
 
 ## now do the same but with generated data
-n <- 1000
+n <- 100
 epsilon.t <- rnorm(n, 0, sigma)
 theta <- 1.17
 Y.t <- theta + epsilon.t
@@ -52,20 +52,20 @@ sigma.w <- sqrt(0.9)
 w.t <- rnorm(n, 0, sigma.w)
 #theta.t <- 
 epsilon.t <- rnorm(n, 0, sigma)
-v.t <- rnorm(1000, 0, 2)
-w.t <- rnorm(1000, 0, 1)
+v.t <- rnorm(n, 0, 2)
+w.t <- rnorm(n, 0, 1)
 
 # allocate memory
-theta.t <- numeric(1000)
-y.t <- numeric(1000)
+theta.t <- numeric(n)
+y.t <- numeric(n)
 
 # set initial value for theta and y
-theta.t[1] <- w.t[1]
-y.t[1] <- theta.t[1] + v.t[1]
-for (i in 2:1000) {
-    theta.t[i] <- theta.t[i-1] + w.t[i]
-    y.t[i] <- theta.t[i] + v.t[i]
+theta.t[1] <- 1.17
+for (i in 2:n) {
+    theta.t[i] <- theta.t[i-1] + v + w.t[i]
 }
+
+y.t <- theta.t + epsilon.t
 
 # priors
 
